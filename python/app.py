747bc21dcae73d7613bb41e29eea5d0be3b84b01
惊蛰 <jingzhe@qidian.local>
2026-06-15 14:55:38 +0800

    CORS放宽为*，修复浏览器跨域问题

--- python/app.py
+++ python/app.py
@@ -52,7 +52,7 @@ app.secret_key = os.environ.get("FLASK_SECRET_KEY") or os.urandom(24).hex()
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "https://123456zjx.pythonanywhere.com"
+    response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     return response
